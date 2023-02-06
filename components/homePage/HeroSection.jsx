3cd1b31b52f19c20626430df6b76a24ce7b85d94
Hardik <sachanhardik@gmail.com>
2023-02-06 20:32:36 +0530

    fix: removed darkmode data from hero tiles

--- components/homePage/HeroSection.jsx
+++ components/homePage/HeroSection.jsx
@@ -32,18 +32,18 @@ export default function HeroSection() {
                     <div
                         className="flex px-8 flex-col w-full justify-between space-y-8 space-x-0 absolute left-0 -bottom-56 sm:space-y-0 sm:space-x-12 sm:-bottom-12 sm:flex-row">
                         <Link href="#"
-                           className="block flex-1 p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-                            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Venue</h5>
-                            <p className="font-normal text-gray-700 dark:text-gray-400">
+                           className="block flex-1 p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100">
+                            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">Venue</h5>
+                            <p className="font-normal text-gray-700">
                                 Auditorium, 5th floor, National Institute of Technology, Delhi
                             </p>
                         </Link>
 
                         <Link href="#"
-                           className="block flex-1 p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-                            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Conference
+                           className="block flex-1 p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100">
+                            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">Conference
                                 Dates</h5>
-                            <p className="font-normal text-gray-700 dark:text-gray-400">
+                            <p className="font-normal text-gray-700">
                                 24 - 25 March, 2023
                             </p>
                         </Link>
