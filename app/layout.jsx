39d031e72aa4c47b2dce08b6ded71529dd976533
Hardik <sachanhardik@gmail.com>
2023-02-08 15:12:16 +0530

    updated header - fix logo on small devices and added contact info

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,13 +1,12 @@
 import './globals.css'
 import NavBar from "@/components/NavBar";
 import Footer from "@/components/Footer";
-import contactheader from"@/components/contactheader";
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head />
       <body className="bg-primary05">
-        <contactheader />
       <NavBar />
       {children}
       <Footer />
--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
-import rtcsseLogo from "@/public/rtcsse-logo-labeled.png"
+import rtcsseLogoLabeled from "@/public/rtcsse-logo-labeled.png"
+import rtcsseLogo from "@/public/rtcsse-logo.png"
 import nitdLogo from "@/public/nitd-logo.png"
 import sscduLogo from "@/public/sscdu-logo.png"
 import Link from "next/link";
@@ -8,18 +9,22 @@ export default function NavBar() {
     return (
         <nav className="sticky top-0 z-50 bg-primary05 drop-shadow">
             <div className="bg-black text-white text-xs">
-                <div className="container mx-auto py-1">
-                    <p className="text-center">Contact us - <Link href="mailto:rtcsee@nitdelhi.ac.in"
-                                                                  className="underline hover:text-primary20">rtcsee@nitdelhi.ac.in</Link>
-                    </p>
+                <div className="container mx-auto py-1 px-6">
+                    <div className="items-center justify-between flex flex-col md:flex-row">
+                        <p className="text-center">Contact us - <span><Link href="mailto:rtcsee@nitdelhi.ac.in"
+                                                                            className="underline hover:text-primary20">rtcsee@nitdelhi.ac.in</Link></span>
+                        </p>
+                        <p>+91 782 854 5356, 011 3386 1250, 011 3386 1256</p>
+                    </div>
                 </div>
             </div>
             <div className="container mx-auto p-6 ">
                 <div className="flex items-center justify-between">
-                    <div className="pt-2">
-                        <Image src={rtcsseLogo} className="max-h-12 object-contain" alt="RTCSSE 2023"/>
+                    <div className="flex pt-2">
+                        <Image src={rtcsseLogoLabeled} className="max-h-12 object-contain hidden md:block" alt="RTCSSE 2023"/>
+                        <Image src={rtcsseLogo} className="max-h-12 object-contain md:hidden" alt="RTCSSE 2023" width={72}/>
                     </div>
-                    <div className="hidden md:flex space-x-4 items-center justify-end">
+                    <div className="flex space-x-4 items-center justify-end">
                         <div className="pt-2">
                             <Image src={nitdLogo} className="max-h-12 object-contain" alt="NITD" width={72} />
                         </div>
