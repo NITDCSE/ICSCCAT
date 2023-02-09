1cedd78d484288051c4047adfe44802d615bd624
Hardik <sachanhardik@gmail.com>
2023-02-09 09:59:04 +0530

    fixed about colleges section in moblie view

--- components/homePage/AboutColleges.jsx
+++ components/homePage/AboutColleges.jsx
@@ -2,21 +2,32 @@ export default function AboutColleges() {
     return (
         <section id="about">
             <div className="bg-primary10">
-                <div className="grid grid-cols-2 gap-x-6 gap-y-3 justify-between px-6 mx-auto py-12 container">
-                    <h3 className=" max-w-md text-2xl font-bold text-center md:text-3xl md:text-left">
-                        About National Institute of Technology, Delhi
-                    </h3>
-                    <h3 className=" max-w-md text-2xl font-bold text-center md:text-3xl md:text-left">
-                        About Swami Shraddhanand College
+                <div className="flex flex-col md:flex-row space-y-6 md:space-y-auto md:space-x-6 justify-between px-6 mx-auto py-12 container">
+                    <div className="flex flex-1 flex-col space-y-3">
+                        <h3 className="text-2xl font-bold text-center md:text-3xl md:text-left">
+                            About National Institute of Technology, Delhi
+                        </h3>
+                        <p className="text-center text-black md:text-left">
+                            National Institute of Technology Delhi is a premier public technical autonomous institution
+                            located in New Delhi. NIT Delhi is an Institution of National Importance (INI) and aims to
+                            provide instructions and research facilities in various disciplines of Engineering, Science
+                            and Technology, Management, Social Sciences and Humanities for advance learning and
+                            dissemination of knowledge. The mission of NIT Delhi is to produce human resource those who
+                            are creative, competitive and innovative with high intellect and ethical values.
+                        </p>
+                    </div>
+                    <div className="flex flex-1 flex-col space-y-3">
+                        <h3 className="text-2xl font-bold text-center md:text-3xl md:text-left">
+                            About Swami Shraddhanand College
 
-                    </h3>
-                    <p className="text-center  text-black md:text-left">
-                        National Institute of Technology Delhi is a premier public technical autonomous institution located in New Delhi. NIT Delhi is an Institution of National Importance (INI) and aims to provide instructions and research facilities in various disciplines of Engineering, Science and Technology, Management, Social Sciences and Humanities for advance learning and dissemination of knowledge. The mission of NIT Delhi is to produce human resource those who are creative, competitive and innovative with high intellect and ethical values.
-                    </p>
-
-                    <p className="text-center  text-black md:text-left">
-                        Swami Sharddhanand College, a constituent College of Delhi University, is run under the trusteeship of Central Govt. It is a co-educational institution and imparts instructions in various subjects at both undergraduate and post-graduate levels. It is equipped with qualified teaching staff, specialized laboratories and adequate library facilities.
-                    </p>
+                        </h3>
+                        <p className="text-center text-black md:text-left">
+                            Swami Sharddhanand College, a constituent College of Delhi University, is run under the
+                            trusteeship of Central Govt. It is a co-educational institution and imparts instructions in
+                            various subjects at both undergraduate and post-graduate levels. It is equipped with
+                            qualified teaching staff, specialized laboratories and adequate library facilities.
+                        </p>
+                    </div>
                 </div>
             </div>
         </section>
