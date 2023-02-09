bfad24e0516aa7484f8986a26e1588812cbb0d58
Hardik <sachanhardik@gmail.com>
2023-02-09 11:24:14 +0530

    updated timeline

--- components/homePage/AboutConference.jsx
+++ components/homePage/AboutConference.jsx
@@ -1,4 +1,12 @@
 export default function AboutConference() {
+    const events = [
+        {name: "Abstract Submission Begins", date: "February 5, 2023"},
+        {name: "Abstract Submission Ends", date: "February 28, 2023"},
+        {name: "Notification of Acceptance", date: "March 6, 2023"},
+        {name: "Last Registration", date: "March 20, 2023"},
+        {name: "Conference", date: "March 24 -25, 2023"},
+    ]
+
     return (
         <>
             <section id="about">
@@ -23,99 +31,34 @@ export default function AboutConference() {
                         Important Dates
                     </h3>
                     <ol className="items-center sm:flex">
-                        <li className="relative mb-6 sm:mb-0">
-                            <div className="flex items-center">
-                                <div
-                                    className="z-10 flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full ring-0 ring-white sm:ring-8 shrink-0">
-                                    <svg aria-hidden="true" className="w-3 h-3 text-blue-800"
-                                         fill="currentColor"
-                                         viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                        <path fill-rule="evenodd"
-                                              d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
-                                              clip-rule="evenodd"></path>
-                                    </svg>
-                                </div>
-                                <div className="hidden sm:flex w-full bg-gray-200 h-0.5 "></div>
-                            </div>
-                            <div className="mt-3 sm:pr-8">
-                                <h3 className="text-lg font-semibold text-gray-900 ">Abstract Submission
-                                    Begins</h3>
-                                <time
-                                    className="block mb-2 text-sm font-normal leading-none text-gray-400">
-                                    February 5, 2023
-                                </time>
-                            </div>
-                        </li>
-                        <li className="relative mb-6 sm:mb-0">
-                            <div className="flex items-center">
-                                <div
-                                    className="z-10 flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full ring-0 ring-white sm:ring-8 shrink-0">
-                                    <svg aria-hidden="true" className="w-3 h-3 text-blue-800"
-                                         fill="currentColor"
-                                         viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                        <path fill-rule="evenodd"
-                                              d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
-                                              clip-rule="evenodd"></path>
-                                    </svg>
-                                </div>
-                                <div className="hidden sm:flex w-full bg-gray-200 h-0.5 "></div>
-                            </div>
-                            <div className="mt-3 sm:pr-8">
-                                <h3 className="text-lg font-semibold text-gray-900 ">Abstract Submission
-                                    ends</h3>
-                                <time
-                                    className="block mb-2 text-sm font-normal leading-none text-gray-400">
-                                    February 28, 2023
-                                </time>
-                            </div>
-                        </li>
-                        <li className="relative mb-6 sm:mb-0">
-                            <div className="flex items-center">
-                                <div
-                                    className="z-10 flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full ring-0 ring-white sm:ring-8 shrink-0">
-                                    <svg aria-hidden="true" className="w-3 h-3 text-blue-800"
-                                         fill="currentColor"
-                                         viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                        <path fill-rule="evenodd"
-                                              d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
-                                              clip-rule="evenodd"></path>
-                                    </svg>
-                                </div>
-                                <div className="hidden sm:flex w-full bg-gray-200 h-0.5 "></div>
-                            </div>
-                            <div className="mt-3 sm:pr-8">
-                                <h3 className="text-lg font-semibold text-gray-900 ">Notification of
-                                    acceptance</h3>
-                                <time
-                                    className="block mb-2 text-sm font-normal leading-none text-gray-400">
-                                    March 6, 2023
-                                </time>
-                            </div>
-                        </li>
-                        <li className="relative mb-6 sm:mb-0">
-                            <div className="flex items-center">
-                                <div
-                                    className="z-10 flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full ring-0 ring-white sm:ring-8 shrink-0">
-                                    <svg aria-hidden="true" className="w-3 h-3 text-blue-800"
-                                         fill="currentColor"
-                                         viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                        <path fill-rule="evenodd"
-                                              d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
-                                              clip-rule="evenodd"></path>
-                                    </svg>
-                                </div>
-                                <div className="hidden sm:flex w-full bg-gray-200 h-0.5"></div>
-                            </div>
-                            <div className="mt-3 sm:pr-8">
-                                <h3 className="text-lg font-semibold text-gray-900">Last
-                                    Registration</h3>
-                                <time
-                                    className="block mb-2 text-sm font-normal leading-none text-gray-400">March
-                                    20,
-                                    2023
-                                </time>
-                            </div>
-                        </li>
+                        {
+                            events.map(function (event) {
+                                return (
+                                    <li className="relative mb-6 sm:mb-0">
+                                        <div className="flex items-center">
+                                            <div
+                                                className="z-10 flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full ring-0 ring-white sm:ring-8 shrink-0">
+                                                <svg aria-hidden="true" className="w-3 h-3 text-blue-800"
+                                                     fill="currentColor"
+                                                     viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                                    <path fill-rule="evenodd"
+                                                          d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
+                                                          clip-rule="evenodd"></path>
+                                                </svg>
+                                            </div>
+                                            <div className="hidden sm:flex w-full bg-gray-200 h-0.5 "></div>
+                                        </div>
+                                        <div className="mt-3 sm:pr-8">
+                                            <h3 className="text-lg font-semibold text-gray-900 ">{event.name}</h3>
+                                            <time
+                                                className="block mb-2 text-sm font-normal leading-none text-gray-400">
+                                                {event.date}
+                                            </time>
+                                        </div>
+                                    </li>
+                                )
+                            })
+                        }
                     </ol>
                 </div>
             </section>
