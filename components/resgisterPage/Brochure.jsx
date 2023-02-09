59bc5a2b047cbfde1d556cd72d9c26f5dd4cd039
Hardik <sachanhardik@gmail.com>
2023-02-09 11:42:41 +0530

    updated brochure

--- components/resgisterPage/Brochure.jsx
+++ components/resgisterPage/Brochure.jsx
@@ -1,10 +1,10 @@
-import rtcssepng from '@/public/rtcssepng.png'
+import brochure from '@/public/brochure.png'
 import Image from 'next/image'
 
 export default function Brochure() {
     return (
         <section id="register" className=" bg-primary10 px-6 py-12 justify-center items-center">
-            <Image src={rtcssepng} alt="" className="h-auto mx-auto" width={1024} />
+            <Image src={brochure} alt="" className="h-auto mx-auto" width={1024} />
         </section>
     )
 }
