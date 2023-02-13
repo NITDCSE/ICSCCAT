57786e792ac6370ab32a4bea2589d770b057e430
Hardik <sachanhardik@gmail.com>
2023-02-14 03:32:50 +0530

    added curtain

--- app/page.jsx
+++ app/page.jsx
@@ -3,7 +3,7 @@ import AboutConference from "@/components/homePage/AboutConference";
 import ConferenceObjectives from "@/components/homePage/ConferenceObjectives";
 import ConferenceThemes from "@/components/homePage/ConferenceThemes";
 import AboutColleges from "@/components/homePage/AboutColleges";
-import Registerbuttonbottom from "@/components/homePage/registerbuttonbottom";
+import CurtainBox from "@/components/curtain/CurtainBox";
 
 export default function Home() {
     return (
@@ -11,11 +11,9 @@ export default function Home() {
             <HeroSection/>
             <AboutColleges/>
             <AboutConference/>
-            <ConferenceObjectives />
-            <ConferenceThemes />
-            
-
-            
+            <ConferenceObjectives/>
+            <ConferenceThemes/>
+            <CurtainBox />
         </>
     )
 }
