7daf37530dec635c1dbff48dd5ea4d9db411f282
Hardik <sachanhardik@gmail.com>
2023-02-13 10:34:34 +0530

    updated footer links

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -11,18 +11,24 @@ export default function Footer() {
         <div className="bg-primary10">
             <div className="container mx-auto px-6 py-8">
                 <div className="grid items-center grid-cols-3">
-                    <a className="m-auto flex flex-col items-center space-y-6" target="_blank" href="https://nitdelhi.ac.in/">
-                        <p className="text-xs text-center text-primaryRegular md:text-lg hidden md:block">DEPARTMENT OF APPLIED SCIENCES</p>
-                        <Image src={nitdLongLogo} alt="" className="max-h-12 object-contain hidden md:block" quality={100}/>
+                    <a className="m-auto flex flex-col items-center space-y-6" target="_blank"
+                       href="https://nitdelhi.ac.in/">
+                        <p className="text-xs text-center text-primaryRegular md:text-lg hidden md:block">DEPARTMENT OF
+                            APPLIED SCIENCES</p>
+                        <Image src={nitdLongLogo} alt="" className="max-h-12 object-contain hidden md:block"
+                               quality={100}/>
                         <Image src={nitdSquareLogo} alt="" className="max-h-12 object-contain md:hidden" quality={100}/>
                     </a>
                     <div className="mx-auto">
                         <Image src={rtcsseLogo} className="object-contain" width={170} quality={100}/>
                     </div>
-                    <a className="m-auto flex flex-col items-center space-y-6" target="_blank" href="https://nitdelhi.ac.in/">
+                    <a className="m-auto flex flex-col items-center space-y-6" target="_blank"
+                       href="https://nitdelhi.ac.in/">
                         <p className="text-xs text-center md:text-lg hidden md:block">DEPARTMENT OF CHEMISTRY</p>
-                        <Image src={sscduLongLogo} alt="" className="max-h-14 object-contain hidden md:block" quality={100}/>
-                        <Image src={sscduSquareLogo} alt="" className="max-h-14 object-contain md:hidden" quality={100}/>
+                        <Image src={sscduLongLogo} alt="" className="max-h-14 object-contain hidden md:block"
+                               quality={100}/>
+                        <Image src={sscduSquareLogo} alt="" className="max-h-14 object-contain md:hidden"
+                               quality={100}/>
                     </a>
                 </div>
             </div>
@@ -30,9 +36,11 @@ export default function Footer() {
                 <div className="container mx-auto px-6 py-2">
                     <p className="text-center">Designed and Developed by - <Link href="https://hardiksachan.com"
                                                                                  className="underline hover:text-primary20">Hardik
-                        Sachan</Link> & <Link
+                        Sachan</Link>, <Link
                         href="https://github.com/ShivanshAsthana04" className="underline hover:text-primary20">Shivansh
-                        Asthana</Link></p>
+                        Asthana</Link> & <Link
+                        href="https://github.com/voltsyjr" className="underline hover:text-primary20">Vishal
+                        Singh</Link></p>
                 </div>
             </div>
         </div>
