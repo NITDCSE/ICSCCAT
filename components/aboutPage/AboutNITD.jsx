fee53073576650e8a437e2344728b5480e551e44
Shivansh <sasthana083@gmail.com>
2023-02-14 02:44:03 +0530

    photos added and website updated

--- components/aboutPage/AboutNITD.jsx
+++ components/aboutPage/AboutNITD.jsx
@@ -25,22 +25,19 @@ export default function AboutNITD() {
                             <div className="flex-1">
                                 <Image src={nitdAdmin} className="object-cover" />
                             </div>
-                           
+
                         </div>
                     </div>
                     <div className="bg-primary10">
-                    <div className="grid grid-cols-2">
-                    <div className="flex-1">
+                        <div className="grid grid-cols-2">
+                            <div className="flex-1">
                                 <Image src={nitdAcad} className="object-cover" />
                             </div>
-                        <div className="flex-1">
-                            <p className="text-center  p-6 py-6 text-black lg:text-lg text-justify">
-                            The institute makes all attempts to establish strong research collaborations in evolving fields of research in science and engineering. The programmes like joint thesis work, inter-institute collaborative projects, institute assistantship to PG and doctoral students, and administrative support to faculty members for taking up research and consultancy work (sponsored by external agencies). From 2022-23 academic session, the institute has started the Post-Doctoral Fellowship (PDF) programme to further escalate the research activities.
-                            NIT Delhi has implemented the guidelines of National Education Policy (NEP) 2020 for current batches of students.
-
-                        </p></div>
-                        
-
+                            <div className="flex-1">
+                                <p className="text-center  p-6 py-6 text-black lg:text-lg md:text-left">
+                                    The institute makes all attempts to establish strong research collaborations in evolving fields of research in science and engineering. The programmes like joint thesis work, inter-institute collaborative projects, institute assistantship to PG and doctoral students, and administrative support to faculty members for taking up research and consultancy work (sponsored by external agencies). From 2022-23 academic session, the institute has started the Post-Doctoral Fellowship (PDF) programme to further escalate the research activities.
+                                    NIT Delhi has implemented the guidelines of National Education Policy (NEP) 2020 for current batches of students.
+                                </p></div>
                         </div>
                     </div>
                     
@@ -49,15 +46,15 @@ export default function AboutNITD() {
                 </div>
         
         </section>
-        <section id="about" className="justify-center flex">
+        <section id="about">
            
         <div className="flex flex-col md:flex-col mt-4  container">
         <h3 className="text-2xl px-6 font-bold text-center md:text-3xl md:text-left">
          About Department of Applied Sciences, NIT Delhi
         </h3>
         <div className="bg-primary10 py-3 mt-10">
-                        <p className="text-center px-6  mx-auto text-black lg:text-lg text-justify">
-                        The Department of Applied Sciences provides an inclusive environment by integrating a range of educational, research and consultancy opportunities to enhance knowledge and skills. The department acts as a bridge between the basic sciences like physics, chemistry and mathematics with engineering procedures. Faculty in applied sciences is involved in teaching high quality basic and advanced courses to the undergraduate students to make them strong in fundamentals related to their concerned engineering disciplines. Also, our faculty provides the graduate students with knowledge and skills by offering specialized courses across a broad range of study options in M. Tech. and Doctoral programmes. The department has its own doctoral programme in applied physics, chemistry, and pure and applied mathematics. The department aspires to develop the full potential of our students whilst offering an enriched educational experience that equips them with the qualifications and skills to achieve their scientific and technical ambitions. The department has several specialized research laboratories, and is all set to start M. Tech. programmes in "Smart Materials and Technology" and "Mathematics and Computation".
+                        <p className="text-center px-6  mx-auto text-black lg:text-lg md:text-left">
+                        The Department of Applied Sciences provides an inclusive environment by integrating a range of educational, research and consultancy opportunities to enhance knowledge and skills. The department acts as a bridge between the basic sciences like physics, chemistry and mathematics with engineering procedures. Faculty in applied sciences is involved in teaching high quality basic and advanced courses to the undergraduate students to make them strong in fundamentals related to their concerned engineering disciplines. Also, our faculty provides the graduate students with knowledge and skills by offering specialized courses across a broad range of study options in M. Tech. and Doctoral programmes. The department has its own doctoral programme in applied physics, chemistry, and pure and applied mathematics. The department aspires to develop the full potential of our students whilst offering an enriched educational experience that equips them with the qualifications and skills to achieve their scientific and technical ambitions.The department has several specialized research laboratories, and is all set to start M. Tech. programmes in "Smart Materials and Technology" and "Mathematics and Computation".
                         </p>
                     </div>
             
