f38bc121a938f7790328ac549fe38b30d0e7933e
Hardik <sachanhardik@gmail.com>
2023-02-14 13:44:59 +0530

    fix about us

--- components/aboutPage/AboutColleges.jsx
+++ components/aboutPage/AboutColleges.jsx
@@ -6,10 +6,9 @@ import {aboutColleges} from "@/components/aboutPage/data";
 import CollegeTabs from "@/components/aboutPage/CollegeTabs";
 
 export default function AboutColleges() {
-    let [nitdActive, setNitdActive] = useState(true)
     return(
         <>
-            <AboutNITD college={nitdActive ? aboutColleges.nitd : aboutColleges.ssdu} nitdActive={nitdActive} setNitdActive={setNitdActive()}/>
+            <AboutNITD college={aboutColleges.nitd} nitdActive={true} setNitdActive={()=> {}}/>
         </>
     )
 }
