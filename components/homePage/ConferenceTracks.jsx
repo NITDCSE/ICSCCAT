e26707cf13261fdc3ac36ad83170853f21dcdcfb
rex <rohitrex321@gmail.com>
2023-09-01 16:24:39 +0530

    fifth draft

--- components/homePage/ConferenceTracks.jsx
+++ components/homePage/ConferenceTracks.jsx
@@ -1,36 +1,63 @@
+import data_analytics from "@/public/Data-analytics.jpg";
+
 export default function ConferenceTracks() {
-    const themes = [
-        "DATA ANALYTICS AND MINING",
-        "CRYPTOGRAPHY, CYBER SECURITY AND NETWORK SECURITY",
-        "CLOUD COMPUTING AND IOT",
-        "ARTIFICIAL INTELLIGENCE AND MACHINE LEARNING",
-        "HEALTHCARE 4.0"
-    ]
-    return (
-        <section id="themes" className="py-6">
-            <div className="container flex flex-col mx-auto px-6 py-4">
-            <h4 className="max-w-md text-4xl font-bold text-center md:text-4xl md:text-left">Tracks</h4>
-            <div className="mt-6 grid grid-cols-1 xs:grid-cols-2 md:grid-cols-3 gap-4">
-                {themes.map(function (theme, index) {
-                return (
-                    <div key={index} className="bg-gray-200 p-4 rounded-lg shadow-md aspect-w-1 aspect-h-1">
-                    <ConferenceTrackGridItem theme={theme} />
-                    </div>
-                );
-                })}
-            </div>
-            </div>
-        </section>
+  const themes = [
+    {
+      name: "DATA ANALYTICS AND MINING",
+      imageUrl: data_analytics,
+    },
+    {
+      name: "CRYPTOGRAPHY, CYBER SECURITY AND NETWORK SECURITY",
+      imageUrl: data_analytics,
+    },
+    {
+      name: "CLOUD COMPUTING AND IOT",
+      imageUrl: data_analytics,
+    },
+    {
+      name: "ARTIFICIAL INTELLIGENCE AND MACHINE LEARNING",
+      imageUrl: data_analytics,
+    },
+    {
+      name: "HEALTHCARE 4.0",
+      imageUrl: data_analytics,
+    },
+  ];
 
-
-    )
+  return (
+    <section id="themes" className="py-6">
+      <div className="container flex flex-col mx-auto px-6 py-4">
+        <h4 className="max-w-md text-4xl font-bold text-center md:text-4xl md:text-left">Tracks</h4>
+        <div className="mt-6 grid grid-cols-1 xs:grid-cols-2 md:grid-cols-3 gap-4">
+          {themes.map(function (theme, index) {
+            return (
+              <div
+                key={index}
+                className="bg-gray-200 p-4 rounded-lg shadow-md flex items-center"
+              >
+                <div className="w-1/2 pr-4">
+                  <img
+                    src={theme.imageUrl.src}
+                    alt={theme.name}
+                    className="w-full h-auto rounded-md"
+                  />
+                </div>
+                <div className="w-1/2">
+                  <ConferenceTrackGridItem theme={theme.name} />
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </section>
+  );
 }
 
-function ConferenceTrackGridItem({theme}) {
-    return (
-        <div
-            className="block font-medium text-md leading-tight rounded px-6 py-3 my-2 py-auto md:mx-4 focus:outline-none focus:ring-0">
-            {theme}
-        </div>
-    )
+function ConferenceTrackGridItem({ theme }) {
+  return (
+    <div className="block font-medium text-md leading-tight">
+      <p className="text-black font-semibold">{theme}</p>
+    </div>
+  );
 }
