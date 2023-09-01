ededf36ad632722d046123d993e69440db9be657
utkarsh3105 <trivediutkarsh31@gmail.com>
2023-09-01 16:11:27 +0530

    utkarsh

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,29 +3,39 @@ import sscduLongLogo from '@/public/sscdu-logo-long.png'
 import rtcsseLogo from '@/public/rtcsse-logo.png'
 import nitdSquareLogo from "@/public/nitd-logo.png"
 import sscduSquareLogo from "@/public/sscdu-logo.png"
+import 'assets/css/footer.css'
 import Link from "next/link";
 import Image from "next/image"
 
 export default function Footer() {
     return (<footer>
-        <div className="bg-primary10">
+        <div className="bg-primary10" id="footer">
             <div className="container mx-auto px-6 py-8">
-                <div className="grid items-center grid-cols-2">
-                    {/* <a className="m-auto flex flex-col items-center space-y-6" target="_blank"
-                       href="https://nitdelhi.ac.in/">
-                        <p className="text-xs text-center text-primaryRegular md:text-lg hidden md:block">DEPARTMENT OF
-                            APPLIED SCIENCES</p>
-                        <Image src={nitdLongLogo} alt="" className="max-h-12 object-contain hidden md:block"
-                               quality={100}/>
-                        <Image src={nitdSquareLogo} alt=""  className="max-h-12 object-contain md:hidden" quality={100}/>
-                    </a> */}
+                <div className="footer_content">
                 <div className="footerAdress">
                     <div className="footerAddress_icon">
-                    <h3 class="Footer_Address_heading__33m0l">Address</h3>
+                    <svg stroke="currentColor" fill="currentColor" stroke-width="0" version="1.1" viewBox="0 0 16 16" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M8 0c-2.761 0-5 2.239-5 5 0 5 5 11 5 11s5-6 5-11c0-2.761-2.239-5-5-5zM8 8c-1.657 0-3-1.343-3-3s1.343-3 3-3 3 1.343 3 3-1.343 3-3 3z"></path></svg>
                     </div>
+                    <div className="footer_address_content">
+                    <h3 class="Footer_Address_heading">Address</h3>
+                    <p class="Footer_Address_line__Tu0_Q undefined">National Institute of Technology Delhi</p>
+                    <p class="Footer_Address_line__Tu0_Q undefined">Plot No. FA7, Zone P1, GT Karnal Rd</p>
+                    <p class="Footer_Address_line__Tu0_Q undefined">Delhi - 110036</p>
+                    </div>
+                    
                 </div>
-                    <div className="mx-auto">
-                        <Image src={rtcsseLogo} className="object-contain" width={170} quality={100}/>
+                    <div className="footer_contact">
+                        <div className="footer_contact_icon">
+                        <svg stroke="currentColor" fill="currentColor" stroke-width="0" version="1.1" viewBox="0 0 16 16" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M11 10c-1 1-1 2-2 2s-2-1-3-2-2-2-2-3 1-1 2-2-2-4-3-4-3 3-3 3c0 2 2.055 6.055 4 8s6 4 8 4c0 0 3-2 3-3s-3-4-4-3z"></path></svg>
+                        </div>
+                        <div className="footer_contact_content">
+                        <h3 class="Footer_ContactUs_heading__F1Du9">Contact Us</h3>
+                        <ul class="contact_list">
+                        <li class="Footer_ContactUs_item__rFApu undefined">+91 941 924 0698</li>
+                        <li class="Footer_ContactUs_item__rFApu undefined">+91 800 338 9258</li>
+                        </ul>
+                        </div>
+                        
                     </div>
                     
                 </div>
