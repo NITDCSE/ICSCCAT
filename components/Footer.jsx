bbc42947037aeddf0349a9b955b8f3d988cdb0cc
utkarsh3105 <trivediutkarsh31@gmail.com>
2023-09-01 15:13:10 +0530

    changes

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -10,26 +10,24 @@ export default function Footer() {
     return (<footer>
         <div className="bg-primary10">
             <div className="container mx-auto px-6 py-8">
-                <div className="grid items-center grid-cols-3">
-                    <a className="m-auto flex flex-col items-center space-y-6" target="_blank"
+                <div className="grid items-center grid-cols-2">
+                    {/* <a className="m-auto flex flex-col items-center space-y-6" target="_blank"
                        href="https://nitdelhi.ac.in/">
                         <p className="text-xs text-center text-primaryRegular md:text-lg hidden md:block">DEPARTMENT OF
                             APPLIED SCIENCES</p>
                         <Image src={nitdLongLogo} alt="" className="max-h-12 object-contain hidden md:block"
                                quality={100}/>
-                        <Image src={nitdSquareLogo} alt="" className="max-h-12 object-contain md:hidden" quality={100}/>
-                    </a>
+                        <Image src={nitdSquareLogo} alt=""  className="max-h-12 object-contain md:hidden" quality={100}/>
+                    </a> */}
+                <div className="footerAdress">
+                    <div className="footerAddress_icon">
+                    <h3 class="Footer_Address_heading__33m0l">Address</h3>
+                    </div>
+                </div>
                     <div className="mx-auto">
                         <Image src={rtcsseLogo} className="object-contain" width={170} quality={100}/>
                     </div>
-                    <a className="m-auto flex flex-col items-center space-y-6" target="_blank"
-                       href="https://ss.du.ac.in/">
-                        <p className="text-xs text-center md:text-lg hidden md:block">DEPARTMENT OF CHEMISTRY</p>
-                        <Image src={sscduLongLogo} alt="" className="max-h-14 object-contain hidden md:block"
-                               quality={100}/>
-                        <Image src={sscduSquareLogo} alt="" className="max-h-14 object-contain md:hidden"
-                               quality={100}/>
-                    </a>
+                    
                 </div>
                 <p className="text-sm text-center mt-12">All right reserved RTCSSE-2023. Powered by NIT Delhi</p>
             </div>
