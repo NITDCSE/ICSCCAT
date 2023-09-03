ee15dd614d526d77f64c561cf6f5916d1ac3a88f
sajal <sajalsahu8@gmail.com>
2023-09-03 12:20:46 +0530

    resolved bugs

--- app/sponsors/page.jsx
+++ app/sponsors/page.jsx
@@ -1,4 +1,4 @@
-import Sponsors from "@/components/sponsorsPage/sponsors";
+import Sponsors from "@/components/sponsorspage/sponsors";
 export default function Sponsor() {
     return ( <>
         <Sponsors/>
