7049f0e80b49c2e3df6cf29c99e9da37d9ad249c
rex <rohitrex321@gmail.com>
2023-09-03 22:38:22 +0530

    final pakka commit

--- components/homePage/AboutConference.jsx
+++ components/homePage/AboutConference.jsx
@@ -8,7 +8,7 @@ export default function AboutConference() {
                         <h3 className="text-3xl font-bold text-center md:text-4xl md:text-left">
                             About the Conference
                         </h3>
-                        <p className="text-center text-black lg:text-lg md:text-justify">
+                        <p className="flex items-center flex-1 flex-col text-justify space-y-3">
                         The area of computing and communication has grown evidently since past two decades with wide ranging aspects. Computing includes designing, developing and building hardware and software systems; processing, structuring, and managing various kinds of information; doing scientific research on and with computers; making computer systems behave intelligently; and creating and using communications and entertainment media.
 
                         The field of computing includes computer engineering, software engineering, computer science, information systems, information technology and the list is virtually endless, and the possibilities are vast. Communication means to share, it is the act of conveying intended meanings from one entity or group to another through the use of mutually understood signs and semiotic rules. It also has wide ranging applied areas which includes Network communication, Security etc
