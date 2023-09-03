df063bc410414aaec02d5117bf66ba35f18255ed
sajal <sajalsahu8@gmail.com>
2023-09-03 12:28:54 +0530

    fixed bug

--- app/page.jsx
+++ app/page.jsx
@@ -3,8 +3,7 @@ import AboutConference from "@/components/homePage/AboutConference";
 import ConferenceObjectives from "@/components/homePage/ConferenceObjectives";
 import ConferenceTracks from "@/components/homePage/ConferenceTracks";
 import AboutColleges from "@/components/homePage/AboutColleges";
-import Registerbuttonbottom from "@/components/homePage/registerbuttonbottom";
-import SponsorsSection from "@/components/homepage/sponsors";
+import SponsorsSection from "@/components/homePage/sponsors";
 
 export default function Home() {
     return (
