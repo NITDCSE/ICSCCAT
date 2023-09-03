88d0d91102d46911026e66b7fe6f45266fa3b081
sajal <sajalsahu8@gmail.com>
2023-09-03 11:56:09 +0530

    updated sponsors

--- app/page.jsx
+++ app/page.jsx
@@ -4,7 +4,7 @@ import ConferenceObjectives from "@/components/homePage/ConferenceObjectives";
 import ConferenceTracks from "@/components/homePage/ConferenceTracks";
 import AboutColleges from "@/components/homePage/AboutColleges";
 import Registerbuttonbottom from "@/components/homePage/registerbuttonbottom";
-import SponsorsSection from "@/components/sponsorspage/sponsors";
+import SponsorsSection from "@/components/homepage/sponsors";
 
 
 export default function Home() {
