b06afd4bdf9ac706bcff09418daaf327f9ff16eb
Sajal <sajalshau8@gmail.com>
2023-09-25 15:17:41 +0530

    addded how to reach

--- app/page.jsx
+++ app/page.jsx
@@ -4,14 +4,19 @@ import ConferenceObjectives from "@/components/homePage/ConferenceObjectives";
 import ConferenceTracks from "@/components/homePage/ConferenceTracks";
 import AboutColleges from "@/components/homePage/AboutColleges";
 import SponsorsSection from "@/components/homePage/sponsors";
-
+import Howtoreach from "@/components/delhi.jsx"
 export default function Home() {
     return (
         <>
             <HeroSection/>
             <AboutColleges/>
             <AboutConference/>
+            <Howtoreach/>
+            
              {/* <SponsorsSection /> */}
+            
+             {/* how to reach */}
+             {/* about delhi */}
         </>
     )
 }
---
+++ components/delhi.jsx
@@ -0,0 +1,38 @@
+import Image from "next/image";
+
+
+export default function Howtoreach() {
+    return (
+        <section id="about" className="bg-primary10">
+            <div className="container mx-auto px-6 py-12">
+                <h2 className="text-3xl md:text-4xl font-bold text-center pb-8">How to Reach</h2>
+                <div className="flex flex-col md:flex-row space-y-6 md:space-y-0 md:space-x-24 justify-between">
+                    <div className="flex flex-1 items-center flex-col space-y-3">
+                        <p className="flex items-center flex-1 text-justify flex-col space-y-3">
+                            
+                            
+                             
+                            <b>The venue for the event i.e., National Institue of Technology Delhi is about 30 kms far away from the center of the city i.e., India Gate. There are several ways you can use to reach the venue. The distances from several means are mentioned below.</b>
+                            <br></br>
+                            <br></br>
+                            
+
+                            1, Indira Gandhi Internal Airport (T-3) – 50 kms
+                            <br></br>
+                            2. Hazrat Nizamuddin Railway Station – 40 kms
+                            <br></br>
+                            3. New Delhi Railway Station – 30 Kms
+                            <br></br>
+                            <br></br>
+                            Mostly, Cabs are available 24/7 and you can also go for metros which are availble from 06:00 AM to 11:00 PM thorugh-out Delhi. Although to reach NIT Delhi via metro you need to come to “Jahangirpuri Metro Station”(nearest metro station) & then take another available option to come to NIT Delhi which is nearly 15 Kms aways.
+                       
+                        </p>
+                        <iframe src=
+                        "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3495.758813003077!2d77.13065917606504!3d28.816260475712802!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390d1b1923ada2e3%3A0x1169930518add2fe!2sNational%20Institute%20of%20Technology%20Delhi!5e0!3m2!1sen!2sin!4v1695635007871!5m2!1sen!2sin" 
+                        width="600" height="450" frameborder="0" style={{border:0}} allowfullscreen="" aria-hidden="false" tabindex="0"></iframe></div>
+                </div>
+            </div>
+        </section>
+
+    )
+}
