01fccfd12ea66163d7cef5c8eb40ad79dd48cc1b
Dishendra <dishendramishra@gmail.com>
2023-09-26 11:46:17 +0530

    updates

--- components/homePage/AboutColleges.jsx
+++ components/homePage/AboutColleges.jsx
@@ -5,11 +5,11 @@ export default function AboutColleges() {
     return (
 <section id="about" className="bg-primary10">
     <div className="container mx-auto px-6 py-12">
-        <h2 className="text-3xl md:text-4xl font-bold text-center pb-8">Organised by</h2>
+        <h2 className="text-2xl md:text-3xl font-bold text-center pb-8">Organised by</h2>
         <div className="flex flex-col md:flex-row space-y-6 md:space-y-0 md:space-x-24 justify-between">
             <div className="flex flex-1 items-center flex-col space-y-3">
-                <p className="text-lg uppercase text-center md:text-xl">Department of Computer Science & Engineering</p>
-                <p className="text-xl uppercase text-center md:text-xl">National Institute of Technology, Delhi</p>
+                <p className="font-bold text-xl uppercase text-center md:text-xl text-blue-700">Department of Computer Science & Engineering</p>
+                <p className="font-bold text-xl uppercase text-center md:text-2xl text-blue-700">National Institute of Technology, Delhi</p>
                 {/* <Image src={nitdLongLogo} height={72} alt="" quality={100} className="max-h-18 object-contain"/> */}
                 <h3 className="text-xl font-bold text-center md:text-2xl pt-4 hidden">
                     National Institute of Technology, Delhi
