462aaad12f86101485c2b0c45fef516e51d90dd1
Dishendra <dishendramishra@gmail.com>
2023-12-20 22:58:23 +0530

    added sponsors

--- components/sponsorspage/sponsors.jsx
+++ components/sponsorspage/sponsors.jsx
@@ -4,30 +4,32 @@ import aegmatel from '@/public/sponsors/aegmatel.jpeg'
 import globus from '@/public/sponsors/globus.jpeg'
 import mathworks from '@/public/sponsors/mathworks.jpeg'
 import takyon from '@/public/sponsors/takyon.jpeg'
+import serb from '@/public/sponsors/serb.png'
+import kcet from '@/public/sponsors/kcet.png'
 
  
 export default function SponsorsSection() {
     return (
         <>
         <h1 className=" bg-primary10 py-3 text-center hed underline">Sponsors</h1>
-        {/* <section id="railway" className=" bg-primary10 px-3 py-12 spon">
+        <section id="railway" className=" bg-primary10 px-3 py-12 spon">
         <div class="card-s">
             <div className="s-logo">
-            <Image src={aegmatel} alt=""/>
+            <Image src={serb} alt=""/>
             </div>
             <div className="text-s">
-                <span className="font-bold">Aegmatel</span> 
+                <span className="font-bold">Science and Engineering Research Board</span> 
             </div>
         </div>
         <div class="card-s">
             <div className="s-logo">
-            <Image src={globus} alt=""/>
+            <Image src={kcet} alt=""/>
             </div>
             <div className="text-s">
-                <span className="font-bold">Globus</span>  
+                <span className="font-bold">Khalsa College Of Engineering & Technology</span>  
             </div>
         </div>
-        <div class="card-s">
+        {/* <div class="card-s">
             <div className="s-logo">
             <Image src={mathworks} alt="" className="cdh"/>
             </div>
@@ -43,10 +45,10 @@ export default function SponsorsSection() {
                 <span className="font-bold">Takyon</span>  
             </div>
         </div>
-        
-          </section> */}
+         */}
+          </section>
 
-        <section id="railway" className=" bg-primary10 px-3 py-12 spon">Under Construction</section>
+        {/* <section id="railway" className=" bg-primary10 px-3 py-12 spon">Under Construction</section> */}
         <br></br>
         </>
     )
