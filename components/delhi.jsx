e4f279e2eda18065ddbc7ef13f30161037741f51
Alok Kumar <alokkumar.ptu@gmail.com>
2024-06-13 01:20:23 +0530

    Address Update

--- components/delhi.jsx
+++ components/delhi.jsx
@@ -12,7 +12,7 @@ export default function Howtoreach() {
                             
                             
                              
-                            <b>The venue for the event i.e., National Institue of Technology Delhi is about 30 kms from the center of the city i.e., India Gate, New delhi. There are several ways you can use to reach the venue. The distances from several means are mentioned below.</b>
+                            <b>The venue for the event i.e., National Institue of Technology Delhi is about 30 kms from the center of the city i.e., India Gate, New delhi FA7,Zone, P1, GT Karnal Rd, Delhi, 110036. There are several ways you can use to reach the venue. The distances from several means are mentioned below.</b>
                             <br></br>
                             <br></br>
                             
